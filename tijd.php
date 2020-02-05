c8ba5be497574c67fdc4ac3592e5f0c212604076
NielzosFilms <nielzosfilms@gmail.com>
2020-02-05 10:14:30 +0100

    Uitwerking van de exercise

--- tijd.php
+++ tijd.php
@@ -4,19 +4,29 @@ $min = 0;
 $hour = 0;
 $dag = 0;
 for($i = 1;$i<count($argv);$i++){
-    if(strpos($argv[$i], "s")) {
-        $arr = explode("s", $argv[$i], 2);
-        $sec = (int)$arr[0];
-    }else if(strpos($argv[$i], "m")) {
-        $arr = explode("m", $argv[$i], 2);
-        $min = (int)$arr[0];
-    }else if(strpos($argv[$i], "h")) {
-        $arr = explode("h", $argv[$i], 2);
-        $hour = (int)$arr[0];
-    }else if(strpos($argv[$i], "d")) {
-        $arr = explode("d", $argv[$i], 2);
-        $dag = (int)$arr[0];
+    $char = substr($argv[$i], strlen($argv[$i])-1);
+    switch($char){
+        case "s":
+            $arr = explode("s", $argv[$i], 2);
+            $sec = (int)$arr[0];
+            break;
+        case "m":
+            $arr = explode("m", $argv[$i], 2);
+            $min = (int)$arr[0];
+            break;
+        case "h":
+            $arr = explode("h", $argv[$i], 2);
+            $hour = (int)$arr[0];
+            break;
+        case "d":
+            $arr = explode("d", $argv[$i], 2);
+            $dag = (int)$arr[0];
+            break;
+        default:
+            echo("Geen tijd gevonden".PHP_EOL);
+            exit();
     }
+    
 }
 $sec += $min * 60;
 $sec += ($hour * 60)*60;
